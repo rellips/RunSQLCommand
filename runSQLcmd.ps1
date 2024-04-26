a24f67d085728083f07bda2de0786f8e85863f2d
Jeff Spiller <rellips@gmail.com>
2024-04-26 15:49:07 -0400

    Update runSQLcmd.ps1

--- runSQLcmd.ps1
+++ runSQLcmd.ps1
@@ -4,7 +4,7 @@
 
 # Licenced under the "BSD 2 Clause" License  https://opensource.org/licenses/BSD-2-Clause
 
-# Copyright 2017 Jeff Spiller Consulting. All rights reserved.
+# Copyright 2024 Jeff Spiller. All rights reserved.
 # Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 # 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
